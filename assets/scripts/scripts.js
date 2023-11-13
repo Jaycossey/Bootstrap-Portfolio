088bca3a9f953e3ea9f536200e7b4ee46d978178
jaycossey <jaycossey@live.com>
2023-11-13 12:10:49 +0000

    added planner project

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -8,3 +8,22 @@ function toggleDropdown() {
 }
 
 toggleButton.addEventListener('click', toggleDropdown);
+
+// Add event listener to pointer,
+// track clientX, clientY
+// create transparent circle div, position should auto update and be on that mouse
+// have the background for that div be a fixed position code image.
+
+// Mousemove code image function 
+const maskDiv = document.createElement('div');
+maskDiv.id = "mask";
+maskDiv.style.border = "2px solid red";
+maskDiv.style.height = "200px";
+maskDiv.style.height = "200px";
+
+// "animate" div on mouse position
+function divAnimate() {
+    maskDiv.positionX = MouseEvent.clientX;
+    maskDiv.positionY = MouseEvent.clientY;
+    
+}
